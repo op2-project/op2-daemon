65c528ea8df1d0ea92c81e5278457c38a1b9dc3a
Tijmen de Mes <t.w.k.demes@gmail.com>
2013-08-31 16:40:59 +0200

    Added account add/delete dialog

--- op2d/web/frontend/templates/assets/js/application.js
+++ op2d/web/frontend/templates/assets/js/application.js
@@ -1,4 +1,50 @@
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+function removeAccount() {
+    console.log($('.selected').html());
+    var account=$('.selected').html();
+    $.fn.dialog2.helpers.confirm("Are you sure to remove account:<br/><p class='text-center text-danger'><strong>" + account + '</strong></p>', {
+        confirm: function() {
+            $.ajax({
+                type: "DELETE",
+                url: "api/v1/accounts/"+ account,
+                contentType: 'application/json; charset=UTF-8',
+                success: function(){
+                    $.fn.dialog2.helpers.alert("The account "+account+" has been removed", {});
+                    getAccounts('account_list','account_info_form');
+                },
+
+            });
+        },
+        decline: function() { alert("You said no? Right choice!"); }
+    });
+
+    event.preventDefault();
+}
+
 function getAccounts(target_id,form) {
+    $('#'+target_id).empty().html(
+        "<li style='margin-right:4px'>"+
+            "<div id='account_buttons' class='btn-group btn-group-xs pull-right'>"+
+                "<button id='add_account' data-toggle='modal' href='#myModal' class='btn btn-primary'><i class='icon-fixed-width icon-plus'></i></button>"+
+                "<button id='remove_account' class='btn btn-default btn-disabled' disabled><i class=' icon-fixed-width icon-minus'></i></button>"+
+            "</div>"+
+        "</li>");
     $.getJSON('api/v1/accounts', function(data) {
         console.log(data);
         $.each(data.accounts, function(index,value) {
@@ -10,6 +56,9 @@ function getAccounts(target_id,form) {
                 $(this).addClass("selected");
             });
             // console.log(value);
+        });
+        $('#remove_account').click(function () {
+            removeAccount();
         });
     });
 }
@@ -90,12 +139,47 @@ $(document).ready(function() {
         }
     });
 
-      $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
         console.log(e.target); // activated tab
         if ( $(e.target).attr('href') == "#system_tab" ) {
-        // if (e.target.id=="system_tab"){
             populateSystemTab();
         }
         //e.relatedTarget // previous tab
     });
+
+        // $("#remove_account_dialog").dialog2({
+        //     showCloseHandle: false,
+        //     removeOnClose: false,
+        //     autoOpen: false,
+        //     closeOnEscape: false,
+        //     closeOnOverlayClick: false
+        // });
+
+        // $('#remove_account').click(function(event) {
+        //     event.preventDefault();
+        //     $("#sample1-dialog").dialog2("open");
+        // });
+
+     $('#account_add_form').on('submit', function(){
+
+        //console.log(JSON.stringify($(this).serializeObject()));
+
+        $.ajax({
+            type: "POST",
+            url: "api/v1/accounts",
+            data: JSON.stringify($(this).serializeObject()),
+            contentType: 'application/json; charset=UTF-8',
+            success: function(){
+                $("#myModal").modal('hide');
+                getAccounts('account_list','account_info_form');
+                return false;
+            },
+            error: function(){
+                console.log("Error");
+                return false;
+            }
+        });
+        return false;
+    });
+
 });
