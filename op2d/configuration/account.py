25c746658c1261591b64fd3f2d8686e7f7b4b70e
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-10-10 10:31:31 +0200

    Disabled SRTP by default

--- op2d/configuration/account.py
+++ op2d/configuration/account.py
@@ -41,7 +41,7 @@ class PSTNSettings(SettingsGroup):
 class RTPSettingsExtension(RTPSettings):
     audio_codec_order = Setting(type=AudioCodecList, default=None, nillable=True)
     inband_dtmf = Setting(type=bool, default=True)
-    srtp_encryption = Setting(type=SRTPEncryption, default='optional')
+    srtp_encryption = Setting(type=SRTPEncryption, default='disabled')
     use_srtp_without_tls = Setting(type=bool, default=True)
 
 
