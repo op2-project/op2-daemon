264cbf084902da14d5a954f09af414dc1906b8ce
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-09-20 19:06:30 +0200

    Move frontend files to resources directory

--- op2d/web/frontend/__init__.py
+++ op2d/web/frontend/__init__.py
@@ -1,13 +1,11 @@
 
-import os
-
 from flask import Flask, render_template
+from op2d.resources import Resources
 
 __all__ = ['app']
 
 
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__, template_folder=Resources.get('frontend'))
 
 @app.route('/')
 def index():
