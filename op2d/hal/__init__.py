7058c0417375f1987364b84f2310e5ad729d1b6a
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-09-20 18:18:57 +0200

    Simplified loading HAL modules

--- op2d/hal/__init__.py
+++ op2d/hal/__init__.py
@@ -1,49 +1,40 @@
 
 from application import log
-from application.notification import IObserver, NotificationCenter
 from application.python import Null
 from application.python.types import Singleton
 from importlib import import_module
-from zope.interface import implements
 
 from op2d.configuration import Configuration
 
 __all__ = ['HardwareAbstractionLayer']
 
 
-class HardwareAbstractionLayer(object):
-    __metaclass__ = Singleton
-    implements(IObserver)
-
-    def __init__(self):
-        self.backend = self._load_backend()
-        self.backend.initialize()
-
-    def _load_backend(self):
-        backend_name = Configuration.hal_backend
-        if backend_name:
-            if '.' not in backend_name:
-                backend_name = 'op2d.hal.backend.' + backend_name
+def load_backend(backend_name):
+    if backend_name:
+        mods = [backend_name]
+        if '.' not in backend_name:
+            mods.insert(0, 'op2d.hal.backend.' + backend_name)
+        excs = []
+        for mod in mods:
             try:
                 backend = import_module(backend_name)
                 return backend.Backend()
             except Exception, e:
-                log.error('Failed to load HAL backend: %s' % e)
-        return Null
+                excs.append(e)
+        log.error('Failed to load HAL backend: %s' % '| '.join(str(e) for e in excs))
+    return Null
+
+
+class HardwareAbstractionLayer(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        self._backend = load_backend(Configuration.hal_backend)
+        self._backend.initialize()
 
     def start(self):
-        self.backend.start()
-        notification_center = NotificationCenter()
-        if self.backend is not Null:
-            notification_center.add_observer(self, sender=self.backend)
+        self._backend.start()
 
     def stop(self):
-        self.backend.stop()
-        notification_center = NotificationCenter()
-        if self.backend is not Null:
-            notification_center.remove_observer(self, sender=self.backend)
-
-    def handle_notification(self, notification):
-        handler = getattr(self, '_NH_%s' % notification.name, Null)
-        handler(notification)
+        self._backend.stop()
 
